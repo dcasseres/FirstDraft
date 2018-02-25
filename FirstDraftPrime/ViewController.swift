54b92ef8d40b36b4fed59dd83b6a7626430e9bec
David Casseres <casseres@mac.com>
2018-02-24 20:02:54 -0800

    Initial Commit

---
+++ FirstDraftPrime/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FirstDraftPrime
+//
+//  Created by David Casseres on 2/24/18.
+//  Copyright © 2018 David Casseres. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
